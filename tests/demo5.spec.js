b9d399fad1d2d4100a91f059d808eeaae43e1c8e
Oliver Liao <rtw0913@gmail.com>
2021-10-25 23:16:42 +0800

    Update demo5.spec.js

--- tests/demo5.spec.js
+++ tests/demo5.spec.js
@@ -11,7 +11,7 @@ const { test, expect } = require('@playwright/test');
 // });
 
 // 第二種方法 重複使用登錄狀態
-test("測試重複使用授權 (reuse signed) 進入到 github 關於我頁面", async ({ page }) => {
+test("重複使用登錄狀態進入到 github 關於我頁面", async ({ page }) => {
     await page.goto('https://github.com/liaooliver');
     // Click text=Oliver Liao
     await expect(page.locator('text=Oliver Liao')).toBeTruthy();
